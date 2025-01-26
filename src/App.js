286fbea1cabbc16ee1a911161d4f74931e7f70a7
mmkvdev <madhuv299@gmail.com>
2025-01-26 18:47:09 +0530

    project: Destructuring at App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Greet from "./components/function/Greet";
+import GreetWithClass from "./components/class/Greet";
 
 function App() {
   return (
     <div className="App">
+      <Greet name="Bruce" />
+      <GreetWithClass name="Clark" />
       <Counter rank={1} />
     </div>
   );
