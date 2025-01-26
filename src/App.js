debd8b8074cc3f9805828a3c6ef2b499030fcbc7
mmkvdev <madhuv299@gmail.com>
2025-01-26 18:46:36 +0530

    project: Counter consumption at App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Greet from "./components/function/Greet";
-import { Greet2 } from "./components/function/Greet";
-import GreetWithClass from "./components/class/Greet";
-import GreetWithoutJSX from "./components/function/GreetWithoutJSX";
-import Welcome from "./components/class/Welcome";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" key="king">
-        <h1>This is children prop from Bruce</h1>
-      </Greet>
-      <Greet name="Clark">
-        <button>This is the submit buttom from clark</button>
-      </Greet>
-      <Greet name="Diana">
-        <p>This is the paragraph from Diana</p>
-      </Greet>
-      <Greet2 />
-      <GreetWithClass name="Bruce" key="king" />
-      <GreetWithClass name="Clark" key="king" />
-      <GreetWithClass name="Diana" key="king" />
-      <GreetWithoutJSX name="Bruce" key="king" />
+      <Counter rank={1} />
     </div>
   );
 }
