107b4d28588a3f31deaf08334d37395f2f86a376
mmkvdev <madhuv299@gmail.com>
2025-01-27 18:13:59 +0530

    concept/styles: modulecss function component

---
+++ src/components/function/ModuleCss.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/moduleStyles.module.css";
+
+function ModuleCss() {
+  return (
+    <div>
+      <h1 className={styles.success}>Module Styles</h1>
+    </div>
+  );
+}
+
+export default ModuleCss;
