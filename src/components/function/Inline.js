bf84a5be4ffb77e2c6a123b4c5808a6ad268429b
mmkvdev <madhuv299@gmail.com>
2025-01-27 18:07:24 +0530

    concept/styles: Inline styles

---
+++ src/components/function/Inline.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const myHeading = {
+  color: "green",
+  fontSize: "10px",
+};
+
+function Inline() {
+  return (
+    <div>
+      <h1 style={myHeading}>Inline Styles</h1>
+    </div>
+  );
+}
+
+export default Inline;
