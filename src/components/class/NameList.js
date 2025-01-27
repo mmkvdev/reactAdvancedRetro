57cdd09656a07fb15aad45a4a92e240b06543166
mmkvdev <madhuv299@gmail.com>
2025-01-27 15:52:48 +0530

    concept/lists: named list component

---
+++ src/components/class/NameList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Person from "../Function/Person";
+
+export class NameList extends Component {
+  render() {
+    const persons = [
+      {
+        ide: 1,
+        name: "MMK",
+        skills: "Reactjs",
+      },
+      {
+        ide: 2,
+        name: "Varma",
+        skills: "Nodejs",
+      },
+    ];
+
+    const personList = persons.map((person) => <Person person={person} />);
+    return <>{personList}</>;
+  }
+}
+
+export default NameList;
