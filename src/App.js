f3b32aa68bfbb40e4765c9d44098be28849d9722
mmkvdev <madhuv299@gmail.com>
2025-01-27 18:06:57 +0530

    concept/Inline: Inline Component at App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/App.css";
 import StyleSheet from "./components/Function/StyleSheet";
+import Inline from "./components/Function/Inline";
 
 function App() {
   return (
     <div className="App">
       <StyleSheet primary />
+      <Inline />
     </div>
   );
 }
