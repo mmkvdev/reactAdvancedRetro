a8c7594ef56d37b6f08e5b2d8674861f0c13c799
mmkvdev <madhuv299@gmail.com>
2025-01-27 18:13:44 +0530

    concept/styles: module css styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import StyleSheet from "./components/Function/StyleSheet";
 import Inline from "./components/Function/Inline";
+import ModuleCss from "./components/Function/ModuleCss";
 
 function App() {
   return (
     <div className="App">
       <StyleSheet primary />
       <Inline />
+      <ModuleCss />
     </div>
   );
 }
