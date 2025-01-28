dbc49814897f7b3718e9e98e7606ac286d8a4e92
mmkvdev <madhuv299@gmail.com>
2025-01-28 16:22:00 +0530

    concept/fragments: Table Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles/App.css";
 import FragmentDemo from "./components/Class/FragmentDemo";
+import Table from "./components/Class/Table";
 
 function App() {
   return (
     <div className="App">
-      <FragmentDemo />
+      <Table />
     </div>
   );
 }
