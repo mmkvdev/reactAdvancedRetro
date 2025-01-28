14840d77b868b9a73138d41204b691ce75edbd67
mmkvdev <madhuv299@gmail.com>
2025-01-28 16:17:26 +0530

    concept/fragments: React Fragments Component

---
+++ src/components/class/FragmentDemo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class FragmentDemo extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Fragment Class Component</div>
+        <h1>Component</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FragmentDemo;
