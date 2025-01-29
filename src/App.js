1ead19e553914a1bdc8d22fc316b20c020117fa9
mmkvdev <madhuv299@gmail.com>
2025-01-29 15:56:39 +0530

    concept/error_boundary: Error Boundary at App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import PortalsParent from "./components/Function/PortalsParent";
+import ErrorBoundary from "./components/Class/ErrorBoundary";
+import Hero from "./components/Function/Hero";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <PortalsParent />
+      <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
     </div>
   );
 }
