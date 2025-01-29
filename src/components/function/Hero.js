e8d6317d64e6485521cc6ba87e90561cad023f6b
mmkvdev <madhuv299@gmail.com>
2025-01-29 16:56:42 +0530

    concept/error_boundary: Hero Component event handler example

--- src/components/function/Hero.js
+++ src/components/function/Hero.js
@@ -4,7 +4,17 @@ function Hero({ heroName }) {
   if (heroName === "Joker") {
     throw new Error("Not a hero!");
   }
-  return <div>{heroName}</div>;
+
+  const clickHandler = () => {
+    throw new Error("click handler");
+  };
+
+  return (
+    <>
+      <div>{heroName}</div>
+      <button onClick={clickHandler}>Event!</button>
+    </>
+  );
 }
 
 export default Hero;
