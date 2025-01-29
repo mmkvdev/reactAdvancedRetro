6e542cfffd05188236475b50cffbd53269c3ae11
mmkvdev <madhuv299@gmail.com>
2025-01-29 18:46:53 +0530

    concept/render_props: App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import ClickCounter from "./components/Class/ClickCounter";
-import HoverCounter from "./components/Class/HoverCounter";
+import ClickCounter2 from "./components/Class/ClickCounter2";
+import Counter from "./components/Class/Counter";
+import HoverCounter2 from "./components/Class/HoverCounter2";
+import User from "./components/Class/User";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <ClickCounter name="MMK" />
-      <HoverCounter />
+      <Counter>
+        {(count, incrementCount) => (
+          <ClickCounter2 count={count} incrementCount={incrementCount} />
+        )}
+      </Counter>
+      <Counter>
+        {(count, incrementCount) => (
+          <HoverCounter2 count={count} incrementCount={incrementCount} />
+        )}
+      </Counter>
+      {/* <ClickCounter2 />
+      <HoverCounter2 />
+      <User render={(isLoggedIn) => (isLoggedIn ? "MMK" : "Guest")} /> */}
     </div>
   );
 }
