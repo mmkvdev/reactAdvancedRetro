727d56ad8257cd0f8b69d9a493e59f7708287786
mmkvdev <madhuv299@gmail.com>
2025-02-02 19:31:35 +0530

    concept/hooks: cleanup with useEffect at Mouse component

--- src/components/function/Effects/Mouse.js
+++ src/components/function/Effects/Mouse.js
@@ -4,13 +4,20 @@ function Mouse() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
+  const logMousePosition = (e) => {
+    console.log("log mouse position");
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+
   useEffect(() => {
     console.log("useEffect called");
-    window.addEventListener("mousemove", (e) => {
-      console.log("log mouse position");
-      setX(e.clientX);
-      setY(e.clientY);
-    });
+    window.addEventListener("mousemove", logMousePosition);
+
+    return () => {
+      console.log("component unmouting code");
+      window.removeEventListener("mousemove", logMousePosition);
+    };
   }, []);
 
   return (
