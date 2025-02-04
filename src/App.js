1402e40cb4a2633227dd94af43a01afc6e4bd940
mmkvdev <madhuv299@gmail.com>
2025-02-04 18:01:24 +0530

    concept/hooks: Data fetching using useState at App

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import React, { useReducer } from "react";
+import React from "react";
 import "./styles/App.css";
-import MoreAdvancedCounter from "./components/Function/Reducer/MoreAdvancedCounter";
-import ComponentA from "./components/Function/Reducer/ComponentA";
-import ComponentB from "./components/Function/Reducer/ComponentB";
-import ComponentC from "./components/Function/Reducer/ComponentC";
-
-export const CountContext = React.createContext();
-
-const initialState = 0;
-
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialState;
-    case "default":
-      return state;
-  }
-};
+import DataFetching1 from "./components/Function/Reducer/DataFetching1";
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
-
   return (
     <div className="App">
-      Count - {count}
-      <CountContext.Provider
-        value={{ countState: count, countDispatch: dispatch }}
-      >
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </CountContext.Provider>
+      <DataFetching1 />
     </div>
   );
 }
