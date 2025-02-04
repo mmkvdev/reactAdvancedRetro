c2244a1de37465c84e130eee755b470d877f5f0a
mmkvdev <madhuv299@gmail.com>
2025-02-04 17:46:46 +0530

    concept/hooks: useReducer with context at App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./styles/App.css";
 import MoreAdvancedCounter from "./components/Function/Reducer/MoreAdvancedCounter";
+import ComponentA from "./components/Function/Reducer/ComponentA";
+import ComponentB from "./components/Function/Reducer/ComponentB";
+import ComponentC from "./components/Function/Reducer/ComponentC";
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+export const CountContext = React.createContext();
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    case "default":
+      return state;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <MoreAdvancedCounter />
+      Count - {count}
+      <CountContext.Provider
+        value={{ countState: count, countDispatch: dispatch }}
+      >
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </CountContext.Provider>
     </div>
   );
 }
