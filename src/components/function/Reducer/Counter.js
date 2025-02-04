0eeecfa8fa0e6e4a6cfcde86c8f0d376997cc91f
mmkvdev <madhuv299@gmail.com>
2025-02-04 16:45:26 +0530

    concept/hooks: useReducer implementation at Counter component

---
+++ src/components/function/Reducer/Counter.js
@@ -0,0 +1,32 @@
+import React, { useState, useReducer } from "react";
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+function Counter() {
+  //   const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, initialState);
+  useReducer();
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
