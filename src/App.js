0284eaa99f0e37d92e621a307080a7db7a31b9fa
mmkvdev <madhuv299@gmail.com>
2025-02-06 19:04:16 +0530

    concept/hooks: useRef hook implementation at App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles/App.css";
-import Counter from "./components/Function/Memo/Counter";
+import FocusInput from "./components/Function/Refs/FocusInput";
+import Timer from "./components/Class/Refs/Timer";
+import FunctionTimer from "./components/Function/Refs/Timer";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Timer />
+      <FunctionTimer />
     </div>
   );
 }
